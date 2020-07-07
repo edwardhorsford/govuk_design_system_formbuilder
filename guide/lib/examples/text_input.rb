e13994a7aa7c68445d4b73f402836a56c47b8f1d
Peter Yates <peter.yates@graphia.co.uk>
2020-07-07 20:10:04 +0100

    Add guide entry for prefixes and suffixes
    Refs #159, #161

--- guide/lib/examples/text_input.rb
+++ guide/lib/examples/text_input.rb
@@ -39,5 +39,14 @@ module Examples
         = f.govuk_text_field :two,    width: 2
       SNIPPET
     end
+
+    def text_field_with_prefix_and_suffix
+      <<~SNIPPET
+        = f.govuk_text_field :price_per_kg,
+          label: { text: 'Price per kilogram' },
+          prefix_text: '£',
+          suffix_text: 'per kg'
+      SNIPPET
+    end
   end
 end
